63609a629d6305e0d3a9a0e9d6809ba373d3bd4f
xXdatelXx <artemgurv@gmail.com>
2023-12-20 13:59:13 -0800

    Add scene with memory allocate

---
+++ Template.Unity/Assets/_Project/Develop/Template/Tools/Unity/Scenes/UnitySceneWithMemoryAllocate.cs
@@ -0,0 +1,30 @@
+using Cysharp.Threading.Tasks;
+
+namespace Template.Tools.Unity
+{
+    public sealed class UnitySceneWithMemoryAllocate : IScene
+    {
+        private readonly IScene _origin;
+        private readonly IScene _empty;
+
+        public UnitySceneWithMemoryAllocate(IScene origin, IScene empty)
+        {
+            _origin = origin;
+            _empty = empty;
+        }
+
+        public string Name => _origin.Name;
+
+        public void Open()
+        {
+            Async().Forget();
+
+            async UniTaskVoid Async()
+            {
+                _empty.Open();
+                await UniTask.NextFrame();
+                _origin.Open();
+            }
+        }
+    }
+}
