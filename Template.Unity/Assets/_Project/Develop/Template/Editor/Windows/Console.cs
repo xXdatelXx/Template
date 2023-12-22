3d3dc799973c337344c9df3693090c9cdcc72f53
xXdatelXx <artemgurv@gmail.com>
2023-12-21 22:09:59 -0800

    Update Console.cs

--- Template.Unity/Assets/_Project/Develop/Template/Editor/Windows/Console.cs
+++ Template.Unity/Assets/_Project/Develop/Template/Editor/Windows/Console.cs
@@ -8,7 +8,7 @@ namespace Template.Editor
     internal static class Console
     {
         [MenuItem(MenuParentItem + " Clear Console " + ClearConsoleKey)]
-        static void ClearConsole()
+        static void Clear()
         {
             var logs = Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
             MethodInfo method = logs.GetMethod("Clear", BindingFlags.Static | BindingFlags.Public);
