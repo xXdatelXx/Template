aa763969c40895eedfb44853b5ad99b1e100542a
xXdatelXx <artemgurv@gmail.com>
2023-12-26 20:29:40 -0800

    Update TimerExtension.cs

--- Template.Unity/Assets/_Project/Develop/Template/Engine/Time/Timer/TimerExtension.cs
+++ Template.Unity/Assets/_Project/Develop/Template/Engine/Time/Timer/TimerExtension.cs
@@ -4,10 +4,10 @@ namespace Template.Engine.Time
 {
     public static class TimerExtension
     {
-        public static async UniTask End(this ITimer t) =>
-            await UniTask.WaitUntil(t.Rest);
-
         public static bool Rest(this ITimer t) =>
             !t.Playing;
+        
+        public static async UniTask End(this ITimer t) =>
+            await UniTask.WaitUntil(t.Rest);
     }
 }
