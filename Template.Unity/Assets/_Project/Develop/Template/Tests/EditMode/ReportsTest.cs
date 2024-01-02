c9b65c1addab986e27ff073f8ff1c187f9bbd6f8
xXdatelXx <artemgurv@gmail.com>
2024-01-01 20:10:53 -0800

    Add Reports tests

---
+++ Template.Unity/Assets/_Project/Develop/Template/Tests/EditMode/ReportsTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Template.Engine.Exceptions;
+using Template.Engine.Time;
+
+namespace Template.Tests.EditMode
+{
+    internal sealed class ReportsTest
+    {
+        [Test]
+        public void LimitReportThrowsExceptionCorrectly()
+        {
+            IReport report = new LimitReport(new EmptyReport(), limit: 5, time: 10);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                for (int i = 0; i <= 10; i++)
+                    report.Send(default);
+            });
+        }
+
+        [Test]
+        public async Task LimitReportResettingWorkCorrectly()
+        {
+            IReport report = new LimitReport(new EmptyReport(), limit: 5, time: 1);
+            var timer = new AsyncTimer(0.5f);
+
+            for (int i = 0; i <= 5; i++)
+                report.Send(default);
+
+            timer.Play();
+            await timer.End();
+
+            Assert.DoesNotThrow(() => report.Send(default));
+        }
+    }
+}
